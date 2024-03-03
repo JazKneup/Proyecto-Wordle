7222541a47cf680e90950a7e9a8f4c18b253ba6f
JazKneup <160659977+JazKneup@users.noreply.github.com>
2024-03-02 22:13:54 -0300

    Poyecto Wordle
    Proyecto final

--- script.js
+++ script.js
@@ -1,73 +1,103 @@
 let intentos = 6;
-let diccionario = ["MONEY", "HAPPY", "ENJOY", "DREAM", "DRINK","START"];
-let random = Math.floor[Math.floor(Math.random() * diccionario.length)];
-const palabra = diccionario[Math.floor(Math.random() * diccionario.length)];
+let tieneVidas = 6;
+let verde = "#79b851";
+let amarillo = "#f3c237";
+let gris = "#a4aec4";
+let diccionario = ["MONEY", "HAPPY", "ENJOY", "DREAM", "DRINK", "START"];
+let palabra = palabraAleatoria(diccionario);
 
-const button = document.getElementById("guess-button");
-const GRID = document.getElementById("grid");
+function palabraAleatoria(diccionario) {
+    let max = diccionario.length - 1;
+    let indice = Math.floor(Math.random() * (max + 1));
+    return diccionario[indice];
+}
 
-console.log(palabra);
-
-button.addEventListener('click', intentar);
-function intentar(){
-    const ROW = document.createElement('div');
-    ROW.className = 'row';
-    const INTENTO = leerIntento();
-
-    console.log(INTENTO);
-    intentos = intentos -1;
-
-    if (INTENTO === palabra){
-        console.log("Ganaste");
-        terminar ("<h1>GANASTE!🥳</h1>")
-        return
-    }else{
-        console.log("analizar intento")
-        for (i in palabra){
-            const SPAN = document.createElement('span')
-            SPAN.className =  "letter";
-           
-    if(palabra[i] === INTENTO[i]){
-        console.log(INTENTO[i], "verde");
-        SPAN.innerHTML = INTENTO[i];
-        SPAN.style.backgroundColor = "green";
-        console.log(SPAN)
-        
+let input = document.getElementById("guess-input");
+input.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById("guess-button").click();
     }
-    else if (palabra.includes(INTENTO[1])){
-        console.log( INTENTO[i], "amarillo");
-        SPAN.innerHTML = INTENTO[i];
-        SPAN.style.backgroundColor = "yellow";
-        console.log(SPAN)
+});
+input.addEventListener("input", () => {
+    if (input.value.length > 5) {
+        alert("Por favor, ingrese solo 5 letras.");
+        input.value = input.value.slice(0, 5); // Limitar a 5 letras
     }
-     else {
-            console.log(INTENTO[i], "gris")
-            SPAN.innerHTML = INTENTO[i];
-            SPAN.style.backgroundColor = "gray";
-            console.log(SPAN)
+});
+
+const API = "https://random-word-api.vercel.app/api?words=1&length=5&type=uppercase";
+fetch(API)
+    .then((response) => {
+        response.json().then((body) => {
+            palabra = body[0].toUpperCase();
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+        palabra = palabraAleatoria(diccionario);
+    });
+
+document.getElementById("guess-button").addEventListener("click", () => {
+    const intento = leerIntento();
+    if (palabra === intento) {
+        terminar("GANASTE!>");
+        return;
+    }
+    const ROW = document.createElement("div");
+    ROW.className = "row";
+    const grid = document.getElementById("grid");
+    for (const i in intento) {
+        const SPAN = document.createElement("span");
+        SPAN.className = "letter";
+        SPAN.innerText = intento[i];
+        if (intento[i] === palabra[i]) {
+            SPAN.style.background = verde;
+        } else if (
+            palabra.includes(intento[i]) &&
+            noSeRepite(palabra, intento[i])
+        ) {
+            SPAN.style.background = amarillo;
+        } else {
+            SPAN.style.background = gris;
         }
         ROW.appendChild(SPAN);
     }
-    GRID.appendChild(ROW);
-  }  
-    if (intentos==0){
-        console.log("PERDISTE!")
-        terminar("<h1>PERDISTE!☠️</h1>")
-       
+    grid.appendChild(ROW);
+    tieneVidas--;
+    if (!tieneVidas) {
+        terminar("GAME OVER!");
+        setTimeout(() => {
+            terminar("La palabra correcta era: " + palabra);
+        }, 2000); // Mostrar el mensaje de la palabra correcta después de 2 segundos (2000 milisegundos)
+        return;
     }
-}
-    function leerIntento(){
-        let intento = document.getElementById("guess-input");
-        intento = intento.value;
-        intento = intento.toUpperCase();
+});
 
-         return intento
-        
-    }
-    function terminar(mensaje){
-        const INPUT = document.getElementById("guess-input");
-        INPUT.disabled = true;
-        BOTON.disabled = true;
-        let contenedor = document.getElementById('guesses');
-        contenedor.innerHTML = mensaje;
-    }
+function leerIntento() {
+    let intento = document.getElementById("guess-input").value.toUpperCase();
+    return intento;
+}
+
+function terminar(mensaje) {
+    document.getElementById("guess-button").disabled = true;
+    let p = document.getElementById("guesses");
+    p.innerHTML = "<h1>" + mensaje + "</h1>";
+    reloadAfterDelay();
+}
+
+function reloadAfterDelay() {
+    setTimeout(() => {
+        // Recargar la página después de 4 segundos (4000 milisegundos)
+        location.reload();
+    }, 10000);
+}
+
+function noSeRepite(palabra, letra) {
+    return palabra.includes(letra);
+}
+
+function reiniciarJuego() {
+    input.value = ""; // Borrar el contenido del campo de entrada
+    input.disabled = false; 
+}
